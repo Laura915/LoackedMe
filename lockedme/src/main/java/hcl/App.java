2c853454a19881fa49aa7a6d1bdcd32d2f506594
Laura <lhernan53112@gmail.com>
2021-01-15 12:04:49 -0500

    Added input control method that closes application

--- lockedme/src/main/java/hcl/App.java
+++ lockedme/src/main/java/hcl/App.java
@@ -1,12 +1,34 @@
 package hcl;
 
+import java.util.Scanner;
+
 public class App 
 {
+    static Scanner scanner = new Scanner(System.in);
+    
     public static void main( String[] args )
     {
     	showWelcomeScreen();
     	showMainMenu();
     }
+    
+    //Collects users input option 
+    private static void collectMainMenuOption() {
+        System.out.println("Please choose 1, 2 or 3:");
+        String option = scanner.nextLine();
+        switch (option) {
+        case "3": //Closes application
+            System.out.println("Thanks for using lockedme.com. Closing application.");
+            System.exit(0);
+            break;
+        default: //Invalid input control
+            System.out.println("Invalid input provided, please choose 1, 2 or 3.");
+            break;
+    }
+    showMainMenu();
+        
+    }
+    
     //Build Main options screen
     private static void showMainMenu() {
         System.out.println("-- MAIN MENU --");
@@ -14,6 +36,7 @@ public class App
         System.out.println("2.) Perform file operations");
         System.out.println("3.) Close the application");
         System.out.println("---------------");
+        collectMainMenuOption();
     }
    
     //Build Welcome screen
