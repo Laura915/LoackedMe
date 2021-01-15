a22823a2f1a61551cb91d57fdfe58bdaeb63266b
Laura <lhernan53112@gmail.com>
2021-01-15 11:45:24 -0500

    Added main menu options

--- lockedme/src/main/java/hcl/App.java
+++ lockedme/src/main/java/hcl/App.java
@@ -5,8 +5,17 @@ public class App
     public static void main( String[] args )
     {
     	showWelcomeScreen();
+    	showMainMenu();
     }
-    
+    //Build Main options screen
+    private static void showMainMenu() {
+        System.out.println("-- MAIN MENU --");
+        System.out.println("1.) Show files in ascending order");
+        System.out.println("2.) Perform file operations");
+        System.out.println("3.) Close the application");
+        System.out.println("---------------");
+    }
+   
     //Build Welcome screen
     private static void showWelcomeScreen() {
         System.out.println("---------------");
