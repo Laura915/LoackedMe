f65f8a80835fcf4a9d6e85bc9344d1e44e10419b
Laura <lhernan53112@gmail.com>
2021-01-15 11:40:08 -0500

    created welcome screen

--- lockedme/src/main/java/hcl/App.java
+++ lockedme/src/main/java/hcl/App.java
@@ -1,13 +1,18 @@
 package hcl;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	showWelcomeScreen();
     }
+    
+    //Build Welcome screen
+    private static void showWelcomeScreen() {
+        System.out.println("---------------");
+        System.out.println("Application: LockedMe.com");
+        System.out.println("Developer: Laura Hernandez");
+        System.out.println("---------------");
+    }
+    
 }
